5b4c0fe51b34464b5663b9d4df1f08cf2cc771c8
EmanueleCozzolino18 <emanuele.cozzolino18@libero.it>
2024-05-12 18:47:10 +0200

    [UPD] DONE EXERCISE

--- js/script.js
+++ js/script.js
@@ -3,3 +3,34 @@
 
 console.log("Lo script funziona correttamente")
 
+const listaSpesa = [
+    'Curry',
+    'Limoni',
+    'Acqua',
+    'Biscotti',
+    'Latte',
+    'Frutta',
+    'Cereali',
+    'Salumi',
+    'Carne'];
+
+let counter = 0;
+
+const ul = document.getElementById('list');
+
+while (counter < listaSpesa.length) {
+
+    const li = document.createElement('li');
+
+    const i = document.createElement('i');
+
+    const h5 = document.createElement('h5');
+
+    h5.innerText = listaSpesa[counter];
+
+    li.append(i, h5)
+
+    ul.append(li);
+
+    counter++;
+}
